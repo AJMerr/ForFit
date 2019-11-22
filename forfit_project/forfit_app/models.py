5225fc13d1bb26b3f67fb4e60fb7f7440026500e
Austin <austin@Austins-MacBook.local>
2019-11-22 16:12:07 -0500

    Added models for Routine, Workouts, and Goal

--- forfit_project/forfit_app/models.py
+++ forfit_project/forfit_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+#Model for Routine
+class Routine(models.Model):
+    name = models.CharField(max_length=100, default="My Routine")
+    week = models.CharField(max_legth=50, default="N/A")
+
+    def __str__(self):
+        return self.name
+
+
+#Model for Workout, extends Routine
+class Workout(models.Model):
+    name = models.CharField(max_length=255)
+    day = models.CharField(max_length=50)
+    description = models.TextField
+    sets = models.IntegerField(max_length=50)
+    reps = models.IntegerField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+#Model for Goals
+class Goal(models.Model):
+    name = models.CharField(max_length=100) 
+    start_weight = models.IntegerField(max_length=10)
+    current_weight = models.IntegerField(max_length=10)
+    end_weight = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return self.name
