4bdaa23368b5c48c668e5d5721ce4dc18392fcf3
Austin <austin@Austins-MacBook.local>
2019-11-25 14:42:17 -0500

    Created a React render for all Routines.
    Also used Async, Await instead of promise functions.

--- client/src/components/AllRoutines.jsx
+++ client/src/components/AllRoutines.jsx
@@ -5,20 +5,34 @@ import { Link } from 'react-router-dom'
 export default class AllRoutines extends Component {
 
     state = {
+        error: "",
         allRoutines: []
     }
 
     componentDidMount() {
-        axios.get('api/v1/routine')
-        .then((res) => {
-            console.log(res.data)
-        })
+        this.fetchRoutines()
+    }
+
+    fetchRoutines = async () => {
+        try {
+            const res = await axios.get("api/v1/routine")
+            this.setState({allRoutines: res.data})
+        }
+        catch (err) {
+            console.log(err)
+            this.setState({error: err.message})
+        }
     }
 
     render() {
         return (
             <div>
-                
+                <h1> All Routines </h1>
+                {this.state.allRoutines.map((routine) =>{
+                    return(
+                        <div> {routine.name} </div>
+                    )
+                })}
             </div>
         )
     }
