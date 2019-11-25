53a32e74558dd17931da60fa7a7a1778d83b1dcc
Austin <austin@Austins-MacBook.local>
2019-11-25 10:46:08 -0500

    Added url paths for RESTful routing

--- forfit_project/forfit_app/urls.py
+++ forfit_project/forfit_app/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 router = routers.DefaultRouter()
 
+
+router = routers.DefaultRouter()
+router.register('routine', views.RoutineView)
+router.register('workout', views.WorkoutView)
+router.register('goal', views.GoalView)
+
+
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
