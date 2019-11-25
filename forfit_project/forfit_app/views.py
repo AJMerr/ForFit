14bb2f128726c21869ef62c0507f1173aeac9be0
Austin <austin@Austins-MacBook.local>
2019-11-25 10:40:27 -0500

    Created viewset

--- forfit_project/forfit_app/views.py
+++ forfit_project/forfit_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import RoutineSerializer, WorkoutSerializer, GoalSerializer
+from .models import Routine, Workout, Goal
 
-# Create your views here.
+
+class RoutineView(viewsets.ModelViewSet):
+    queryset = Routine.objects.all()
+    serializer_class = RoutineSerializer
+
+
+class WorkoutView(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+
+class GoalView(viewsets.ModelViewSet):
+    queryset = Goal.objects.all()
+    serializer_class = GoalSerializer
